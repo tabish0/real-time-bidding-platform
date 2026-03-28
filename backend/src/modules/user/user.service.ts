e31c2ebecc679632289e2f525b23927305111bc6
Tabish Javed <tabish.javed@dubizzlelabs.com>
2026-03-28 14:28:52 +0500

    feat(backend): add user module with entity, service, controller, and unit tests

---
+++ backend/src/modules/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository
+      .createQueryBuilder('user')
+      .orderBy("(regexp_replace(user.name, '\\D', '', 'g'))::int", 'ASC', 'NULLS LAST')
+      .getMany();
+  }
+}
